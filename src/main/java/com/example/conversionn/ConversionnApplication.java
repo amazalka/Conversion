fa19077f34d0b2c39a5544767adffff0c374eb82
Azaliya <ziganshina.azaliya1@gmail.com>
2026-05-04 20:34:08 +0300

    initial commit

---
+++ src/main/java/com/example/conversionn/ConversionnApplication.java
@@ -0,0 +1,13 @@
+package com.example.conversionn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversionnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversionnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/conversionn/ConversionnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conversionn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
